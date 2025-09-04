cc44f412f4c1144e466bcff256252b293a516ee7
Hadis <79583854+HadisZare12@users.noreply.github.com>
2025-09-04 13:16:29 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,3 +1,18 @@
+/*
+============================================================
+Create Database and Schemas
+============================================================
+script purpose:
+This script create a new DB named 'DataWarehouse' after checking if it already exists.
+If the dataset exists, it is dropped and recreated. Additionally, the script sets up three
+schemas within the database: 'bronze','silver', 'gold'.
+
+WARNING:
+Running this script will drop the entire 'DataWarehouse' database if it exists.
+All data in the database will be permanently deleted.Proceed with caution and ensure you have proper
+backups before running this script.
+*/
+
 USE master;
 GO
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
